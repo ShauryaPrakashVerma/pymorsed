db7b730c9555e32c6227341c1302febf82396e9d
Shaurya Prakash Verma <shauryaverma22@gmail.com>
2026-05-01 20:11:50 +0530

    Include the tests for encoder and decoder.

---
+++ src/tests/test_decoder.py
@@ -0,0 +1,32 @@
+from Morse_Code_Generator.decoder import decode
+import pytest
+
+
+def test_decode_hello_world():
+    assert decode(".... . .-.. .-.. --- / .-- --- .-. .-.. -..", "english" ) == "HELLO WORLD"
+
+def test_decode_numeric():
+    assert decode(".---- ..--- ...-- ....- .....") == '12345'
+
+def test_decode_alpha_numeric():
+    assert decode(".... . .-.. .-.. --- / .---- ..--- ...--") == 'HELLO 123'
+
+def test_decode_empty_string():
+    assert decode("") == ""
+
+def test_decode_invalid_character():
+    with pytest.raises(ValueError) as exc:
+        decode("... --- ... #")
+    assert str(exc.value) == "Morse '#' not recognized"
+    
+def test_decode_unknown_pattern():
+    with pytest.raises(ValueError):
+        decode("..-.-.-")
+        
+def test_decode_random_string():
+    with pytest.raises(ValueError):
+        decode("hello")
+        
+def test_decode_invalid_separator():
+    with pytest.raises(ValueError):
+        decode("... --- ... | ...")
--- src/tests/test_encoder.py
+++ src/tests/test_encoder.py
@@ -1,2 +1,23 @@
 from Morse_Code_Generator.encoder import encode
+import pytest
 
+
+def test_encode_hello_world():
+    result = encode("Hello World", "english")
+    assert result == ".... . .-.. .-.. --- / .-- --- .-. .-.. -.."
+
+def test_encode_numeric_message():
+    result = encode("12345", "english")
+    assert result == ".---- ..--- ...-- ....- ....."
+
+def test_encode_mixed_message():
+    result = encode("Hello 123", "english")
+    assert result == ".... . .-.. .-.. --- / .---- ..--- ...--"
+
+def test_encode_empty_string():
+    result = encode("", language="english")
+    assert result == ""
+
+def test_encode_invalid_character():
+    with pytest.raises(ValueError):
+        encode("Hello#", language="english")
